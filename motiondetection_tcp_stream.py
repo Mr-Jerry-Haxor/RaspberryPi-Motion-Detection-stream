86f5d2f6c370349b16b7df4751d62cdd08102884
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-09-03 15:17:35 +0530

    Update motiondetection_tcp_stream.py

--- motiondetection_tcp_stream.py
+++ motiondetection_tcp_stream.py
@@ -1,13 +1,11 @@
 import cv2
 import mediapipe as mp
-from datetime import datetime
 import socket
 import json
 import threading
 
 # Initialize MediaPipe Pose solution
 mp_pose = mp.solutions.pose
-mp_draw = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
 
 # RTSP stream URL (localhost)
@@ -18,7 +16,6 @@ cap = cv2.VideoCapture(rtsp_url)
 
 # Mapping landmark indices to their corresponding names
 landmark_names = mp_pose.PoseLandmark
-
 
 # TCP server setup
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,14 +30,12 @@ def broadcast_data(data):
         except:
             clients.remove(client)
             
-            
 # Accepting clients in a separate thread
 def accept_clients():
     while True:
         client_socket, addr = server_socket.accept()
         clients.append(client_socket)
         print(f"New connection from {addr}")
-        # threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 def main():
     if not cap.isOpened():
@@ -63,13 +58,7 @@ def main():
         # Perform pose detection
         results = pose.process(img_rgb)
         
-        # Draw the detected pose on the original video stream
         if results.pose_landmarks:
-            
-            #drawing lines and points on image
-            # mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_draw.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2),mp_draw.DrawingSpec(color=(255, 0, 255), thickness=2, circle_radius=2))
-            
-            # Print landmark names and their corresponding coordinates
             keypoints = {}
             for idx, landmark in enumerate(results.pose_landmarks.landmark):
                 name = landmark_names(idx).name
@@ -78,15 +67,8 @@ def main():
 
             # Convert the keypoints dictionary to a JSON string
             keypoints_json = json.dumps(keypoints, indent=4)
-            
-            # print(keypoints_json)
             broadcast_data(keypoints_json)
-            
-            # Print current timestamp
-            # print(datetime.now().strftime("%H:%M:%S.%f"))
 
-        # Display the frame
-        # cv2.imshow('Frame', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
